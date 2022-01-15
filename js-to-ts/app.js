634475976bf1dbb87dc4e9424885c0cce5c7d6e5
MasonWho <mason@syftanalytics.com>
2022-01-15 12:33:30 +0200

    refined routes

--- js-to-ts/app.js
+++ js-to-ts/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const routeOneRouter = require("./routes/route_one");
+const routeTwoRouter = require("./routes/route_two");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/one", routeOneRouter);
+app.use("/two", routeTwoRouter);
 
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`);
