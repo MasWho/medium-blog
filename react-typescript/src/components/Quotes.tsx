43ceaec2fc84d94be022b68ae546da923caeb4ae
MasonWho <mason@syftanalytics.com>
2022-02-27 09:00:53 +0200

    Added app styling

--- react-typescript/src/components/Quotes.tsx
+++ react-typescript/src/components/Quotes.tsx
@@ -1,5 +1,30 @@
+import styles from '../styles/Quotes.module.css';
+
+const quotes = [
+  {id: 1, author: 'Mason', body: 'asdflkjadshflkjhqwopeirqepwads;lkfjasl;dkfasdj;f'},
+  {id: 2, author: 'Shweta', body: 'asdflkjadshflkjhqwopeirqepwads;lkfjasl;dkfasdj;f'},
+];
+
+// TODO: put this in a data class maybe
+const Quote: React.FC<{quote: {id: number; author: string, body: string}}> = ({quote}) => {
+  return (
+    <div className={styles.quote}>
+      <p><i>{quote.body}</i></p>
+      <p>- {quote.author}</p>
+    </div>
+  );
+};
+
 const Quotes = () => {
-  return <div></div>;
+  const allQuotes = quotes.map(quote => {
+    return <Quote quote={quote} />
+  });
+
+  return (
+    <div className={styles.quotesContainer}>
+      {allQuotes}
+    </div>
+  );
 };
 
 export default Quotes;
--- react-typescript/src/components/QuotesForm.tsx
+++ react-typescript/src/components/QuotesForm.tsx
@@ -3,10 +3,14 @@ import styles from '../styles/QuotesForm.module.css';
 const QuotesForm = () => {
   return (
     <form className={styles.quotesForm}>
-      <label htmlFor="author">Author</label>
-      <input id="author" type="text" placeholder="Name" />
-      <label htmlFor="quote">Quote</label>
-      <textarea id="quote" placeholder="Quote body" />
+      <div className={styles.inputSection}>
+        <label htmlFor="author">Author</label>
+        <input id="author" type="text" placeholder="Name" />
+      </div>
+      <div className={styles.inputSection}>
+        <label htmlFor="quote">Quote</label>
+        <textarea id="quote" placeholder="Quote body" />
+      </div>
       <button type="submit">Add</button>
     </form>
   );
