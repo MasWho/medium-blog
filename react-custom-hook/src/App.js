120bf8dd09eef223cfa985998e45bb35fc59c659
MasonWho <mason@syftanalytics.com>
2022-08-14 17:13:03 +0200

    complete

--- react-custom-hook/src/App.js
+++ react-custom-hook/src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import { useEffect, useCallback } from 'react';
 import useHttp from './hooks/use-http';
 
+const API_URL = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+
 function App() {
-  const {data, error, loading, request} = useHttp();
+  const {data, loading, error, getData} = useHttp();
+
+  const successResponseHandler = useCallback((data) => {
+    return data.quotes;
+  }, []);
+
+  useEffect(() => {
+    getData(API_URL, null, { 
+      handleSuccessResponse: successResponseHandler 
+    });
+  }, [getData, successResponseHandler]);
+
+  let content;
+  if(loading) {
+    content = <div>Busy Loading...</div>;
+  } else if (error) {
+    content = <div>{error}</div>;
+  } else if (data) {
+    content = <ul>
+      {data.map((quote, idx) => <li key={`${quote.author}-${idx}`}>{quote.text}</li>)}
+    </ul>;
+  }
   
   return (
-    <div className="App">
+    <div>
+      {content}
     </div>
   );
 }
--- react-custom-hook/src/hooks/use-http.js
+++ react-custom-hook/src/hooks/use-http.js
@@ -1,50 +1,35 @@
 import { useState, useCallback } from "react"
 
-/**
- * Custom hook for making http requests using the Fetch API.
- * Provides state logic for loading and error.
- */
 const useHttp = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  /**
-   * send request to a url specified with params passed.
-   * Optionally pass in a call back to handle the response received.
-   * @param {String} url 
-   * @param {Object} params 
-   * @param {Function} handleSuccessResponse 
-   * @param {Function} checkForError 
-   * @param {Function} handleErrorResponse 
-   */
-  const request = useCallback(async (url, params, {handleSuccessResponse, checkForError, handleErrorResponse}) => {
+  const getData = useCallback(async (url, params, {handleSuccessResponse, handleErrorResponse}) => {
     setLoading(true);
-    setError(null);
-
     try {
-      const response = await fetch(url, {...params});
-      const data = await response.json();  // Assume always json response
-      
-      checkForError && checkForError(data);
-
-      // If response is okay and no errors, then successful request
-      handleSuccessResponse && await handleSuccessResponse(data);
-      setData(data);
+      const response = await fetch(url, params);
+      const data = await response.json();
+      if(data.status >= 200 && data.status < 400) {
+        if(handleSuccessResponse) {
+          const transformedData = handleSuccessResponse(data);
+          setData(transformedData);
+        }
+      } else {
+        throw new Error('Request Error!');
+      }
     } catch (error) {
-      // Handle error if specified
-      handleErrorResponse && handleErrorResponse(data, error);
-      setError(error);
+      setError(error.message || error);
+      handleErrorResponse && handleErrorResponse(error);
     }
-
     setLoading(false);
   }, []);
 
   return {
-      loading: loading,
-      error: error,
-      data: data,
-      request: request,
+    loading: loading,
+    error: error,
+    data: data,
+    getData: getData,
   };
 };
 
