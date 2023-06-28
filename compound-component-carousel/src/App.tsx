5f6ccb4ae8592bcc78589c30f3d903951f98f2dc
MasonWho <mason@syftanalytics.com>
2023-06-28 21:42:59 +0200

    Update App.tsx

--- compound-component-carousel/src/App.tsx
+++ compound-component-carousel/src/App.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from "react";
 import "./App.css";
 import Carousel from "./carousel/Carousel";
 
 function App() {
-  const images: JSX.IntrinsicElements['img'][] = [
+  const images: ReactNode[] = [
     <img src="https://picsum.photos/800/300/?random" alt="1" />,
     <img src="https://picsum.photos/800/301/?random" alt="2" />,
     <img src="https://picsum.photos/800/302/?random" alt="3" />,
