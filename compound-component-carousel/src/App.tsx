d9c9eb74c18ede3ea8d1e9a35f8530a603059bed
MasonWho <mason@syftanalytics.com>
2023-06-28 21:40:27 +0200

    refactored original component

--- compound-component-carousel/src/App.tsx
+++ compound-component-carousel/src/App.tsx
@@ -2,17 +2,17 @@ import "./App.css";
 import Carousel from "./carousel/Carousel";
 
 function App() {
-  const slides = [
+  const images: JSX.IntrinsicElements['img'][] = [
     <img src="https://picsum.photos/800/300/?random" alt="1" />,
     <img src="https://picsum.photos/800/301/?random" alt="2" />,
-    // <img src="https://picsum.photos/800/302/?random" alt="3" />,
-    // <img src="https://picsum.photos/800/303/?random" alt="4" />,
-    // <img src="https://picsum.photos/800/304/?random" alt="5" />,
+    <img src="https://picsum.photos/800/302/?random" alt="3" />,
+    <img src="https://picsum.photos/800/303/?random" alt="4" />,
+    <img src="https://picsum.photos/800/304/?random" alt="5" />,
   ];
 
   return (
     <div className="main">
-      <Carousel inputSlides={slides} arrows arrowBorders />
+      <Carousel images={images} arrows arrowBorders autoplay interval={2000}/>
     </div>
   );
 }
--- compound-component-carousel/src/carousel/Carousel.tsx
+++ compound-component-carousel/src/carousel/Carousel.tsx
@@ -1,177 +1,136 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, ReactNode } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./styles/carousel.scss";
 
 type CarouselProps = {
-  inputSlides: React.ReactNode[];
+  images: ReactNode[];
   autoplay?: boolean;
   interval?: number;
-  onSlideChange?: (currSlideIndex: number) => void;
   arrows?: boolean;
   arrowBorders?: boolean;
+  height?: string;
 }
 
-const initialSlides = (slides: any) => {
-  const locSlides: any = [];
-  slides.forEach((slide: any) => {
-    const slideobject = {
-      class: "slider-single proactivede",
-      element: slide,
-    };
-    locSlides.push(slideobject);
-  });
-  if (slides.length === 2) {
-    slides.forEach((slide: any) => {
-      const slideobject = {
-        class: "slider-single proactivede",
-        element: slide,
-      };
-      locSlides.push(slideobject);
-    });
+type Slide = {
+  class: string;
+  element: ReactNode;
+}
+
+const initialSlides = (images: ReactNode[]): Slide[] => {
+  let lessThanThree = images.length < 3;
+  const slides: Slide[] = [];
+  while(lessThanThree) {  
+    images = images.concat(images);
+    lessThanThree = images.length < 3;
   }
-  return locSlides;
+
+  for(let idx = 0; idx < images.length; idx++) {
+    let slideClass = "slider-single proactivede";
+    slides.push({
+      class: slideClass,
+      element: images[idx]
+    })
+  }
+  return slides;
 }
 
 const Carousel = (props: CarouselProps) => {
-  const {inputSlides, arrows, arrowBorders, interval, autoplay, onSlideChange} = props;
-  const [slideCurrent, setSlideCurrent] = useState(-1);
-  const [slides, setSlides] = useState(initialSlides.bind(null, inputSlides));
-  const [height, setHeight] = useState("0px");
-  const intervalRef = useRef<any>(null);
-  const nextRef = useRef<any>();
+  const {images, arrows, arrowBorders, interval, autoplay, height = '300px'} = props;
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const [slides, setSlides] = useState(initialSlides.bind(null, images));
+  const intervalRef = useRef<number>();
+  const nextRef = useRef<HTMLDivElement>(null);
 
   // First render will initialise some styling
   useEffect(() => {
-    if (slideCurrent === -1) {
-      setTimeout(() => {
-        nextRef.current.click();
-        if (autoplay) {
-          intervalRef.current = setTimeout(() => {
-            nextRef.current.click();
-          }, interval);
-        }
-      }, 500);
+    slideRight();
+
+    // Autoplay needs to use a ref or else the sliding function will have sticky states due to closure
+    if(autoplay) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(() => {
+        nextRef.current?.click();
+      }, interval);
     }
-  }, [slideCurrent]);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  }, [nextRef.current]);
 
   const slideRight = () => {
     const activeClass = "slider-single active";
-    const slide: any = [...slides];
-    const lastSlideIndex = slides.length - 1;
-    if (lastSlideIndex > 1) {
-      let slideCurrentLoc = slideCurrent;
-      if(slideCurrentLoc === -1) {
-        slideCurrentLoc = 0;
-      }
-      slideCurrentLoc = slideCurrentLoc % lastSlideIndex;
-      const activeSlide: any = slide[slideCurrentLoc];
-      let preactiveSlide: any;
-      if (slideCurrentLoc > 0) {
-        preactiveSlide = slide[slideCurrentLoc - 1];
-      } else {
-        preactiveSlide = slide[lastSlideIndex];
-      }
+    const updatedSlides = [...slides];
+    const numSlides = updatedSlides.length;
+    const lastSlideIndex = numSlides - 1;
+    let nextSlideIndex = currentSlideIndex + 1;
+    if (currentSlideIndex === lastSlideIndex) {
+      nextSlideIndex = 0;
+    }
 
-      let proactiveSlide: any;
-      // const activeSlide: any = slide[slideCurrentLoc];
-      if (slideCurrentLoc < lastSlideIndex) {
-        proactiveSlide = slide[slideCurrentLoc + 1];
-      } else {
-        proactiveSlide = slide[0];
-      }
+    const previousSlide = updatedSlides[currentSlideIndex];
+    const activeSlide = updatedSlides[nextSlideIndex];
+    // If next slide index overflows, set to first slide
+    const nextSlide = updatedSlides[nextSlideIndex + 1 > lastSlideIndex ? 0 : nextSlideIndex + 1];
 
-      slide.forEach((slid: any) => {
-        if (slid.class.includes("preactivede")) {
-          slid.class = "slider-single proactivede";
-        }
-        if (slid.class.includes("preactive")) {
-          slid.class = "slider-single preactivede";
-        }
-      });
+    updatedSlides.forEach((slide) => {
+      if (slide.class.split(" ").includes("preactivede")) {
+        slide.class = "slider-single proactivede";
+      }
+      if (slide.class.split(" ").includes("preactive")) {
+        slide.class = "slider-single preactivede";
+      }
+    });
 
-      preactiveSlide.class = "slider-single preactive";
-      activeSlide.class = activeClass;
-      proactiveSlide.class = "slider-single proactive";
-      setSlides(slide);
-      setSlideCurrent(slideCurrentLoc + 1);
+    previousSlide.class = "slider-single preactive";
+    activeSlide.class = activeClass;
+    nextSlide.class = "slider-single proactive";
+    
+    setSlides(updatedSlides);
+    setCurrentSlideIndex(nextSlideIndex);
 
-      if (document.getElementsByClassName("slider-single active").length > 0) {
-        setTimeout(() => {
-          if (document.getElementsByClassName("slider-single active").length > 0) {
-            const height = document.getElementsByClassName("slider-single active")[0].clientHeight;
-            setHeight(`${height}px`);
-          }
-        }, 500);
-      }
-      if(onSlideChange) {
-        onSlideChange(slideCurrentLoc);
-      }
-      if (autoplay) {
-        clearTimeout(intervalRef.current);
-        intervalRef.current = setTimeout(() => {
-          nextRef.current.click();
-        }, interval);
-      }
-    } else if (slide[0] && slide[0].class !== activeClass) {
-      slide[0].class = activeClass;
-      setSlides(slide);
-      setSlideCurrent(0);
+    if (autoplay) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(() => {
+        nextRef.current?.click();
+      }, interval);
     }
   };
 
   const slideLeft = () => {
-    const slideTotal = slides.length - 1;
-    if (slideTotal > 1) {
-      let preactiveSlide: any;
-      let proactiveSlide: any;
-      let slideCurrentLoc = slideCurrent;
-      const slide: any = [...slides];
-      if (slideCurrentLoc > 0) {
-        slideCurrentLoc--;
-      } else {
-        slideCurrentLoc = slideTotal;
-      }
+    const numSlides = slides.length;
+    let nextSlideIndex = currentSlideIndex - 1;
+    const lastSlideIndex = numSlides - 1;
+    const updatedSlides = [...slides];
 
-      if (slideCurrentLoc < slideTotal) {
-        proactiveSlide = slide[slideCurrentLoc + 1];
-      } else {
-        proactiveSlide = slide[0];
-      }
-      let activeSlide = slide[slideCurrentLoc];
-      if (slideCurrentLoc > 0) {
-        preactiveSlide = slide[slideCurrentLoc - 1];
-      } else {
-        preactiveSlide = slide[slideTotal];
-      }
-      slide.forEach((slid: any) => {
-        if (slid.class.includes("proactivede")) {
-          slid.class = "slider-single preactivede";
-        }
-        if (slid.class.includes("proactive")) {
-          slid.class = "slider-single proactivede";
-        }
-      });
-      preactiveSlide.class = "slider-single preactive";
-      activeSlide.class = "slider-single active";
-      proactiveSlide.class = "slider-single proactive";
-      setSlides(slide);
-      setSlideCurrent(slideCurrentLoc);
-      if(onSlideChange) {
-        onSlideChange(slideCurrentLoc);
-      }
-      if (document.getElementsByClassName("slider-single active").length > 0) {
-        setTimeout(() => {
-          if (document.getElementsByClassName("slider-single active").length > 0) {
-            const height = document.getElementsByClassName("slider-single active")[0].clientHeight;
-            setHeight(`${height}px`);
-          }
-        }, 500);
-      }
+    if (currentSlideIndex === 0) {
+      nextSlideIndex = lastSlideIndex;
     }
+
+    // If prev slide index overflows, set to last slide
+    const previousSlide = updatedSlides[nextSlideIndex - 1 < 0 ? lastSlideIndex : nextSlideIndex - 1];
+    const activeSlide = updatedSlides[nextSlideIndex];
+    const nextSlide = updatedSlides[currentSlideIndex];
+
+    slides.forEach((slide) => {
+      if (slide.class.split(" ").includes("proactivede")) {
+        slide.class = "slider-single preactivede";
+      }
+      if (slide.class.split(" ").includes("proactive")) {
+        slide.class = "slider-single proactivede";
+      }
+    });
+
+    previousSlide.class = "slider-single preactive";
+    activeSlide.class = "slider-single active";
+    nextSlide.class = "slider-single proactive";
+
+    setSlides(updatedSlides);
+    setCurrentSlideIndex(nextSlideIndex);
   };
 
-  const sliderClass = (direction: any) => {
+  const sliderClass = (direction: 'left' | 'right') => {
     let sliderClass = `slider-${direction}`;
     if (!arrows) {
       sliderClass = "slider-disabled";
@@ -186,8 +145,8 @@ const Carousel = (props: CarouselProps) => {
     content = (
       <div className="slider-container">
         <div className="slider-content">
-          {slides.map((slider: any, index: number) => (
-            <div className={slider.class} key={index}>
+          {slides.map((slide, index: number) => (
+            <div className={slide.class} key={index}>
               <div className={sliderClass("left")} onClick={slideLeft}>
                 <div>
                   <FontAwesomeIcon icon={faArrowLeft} />
@@ -199,7 +158,7 @@ const Carousel = (props: CarouselProps) => {
                 </div>
               </div>
 
-              <div className="slider-single-content">{slider.element}</div>
+              <div className="slider-single-content">{slide.element}</div>
             </div>
           ))}
         </div>
