01bb3a9602760b1aec8fbcf4b2b46d20c48ff27a
MasonWho <mason@syftanalytics.com>
2023-07-25 22:13:22 +0200

    initial commit

--- js-to-ts/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express, {Request, Response, NextFunction} from 'express';
-var router = express.Router();
-
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.send("This is the index page");
-});
-
-export default router;
